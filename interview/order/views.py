312b20b279680e68a0d888e76c5374cefcc47b0a
Afeefa Zaman <engr.afeefa@gmail.com>
2024-01-31 11:04:10 -0800

    Completed challenge 2

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from rest_framework import generics
-
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
@@ -13,3 +15,15 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class DeactivateOrderView(APIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def patch(self, request:Request, *args, **kwargs) -> Response:
+        order = get_object_or_404(self.queryset, pk=kwargs.get('pk'))
+        order.is_active = False
+        order.save()
+
+        return Response({"message": "Order set to inactive"}, status=200)
